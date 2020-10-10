32bef5c419d415e8ea3afced9890e1f2a316fd0c
Ilya Malanin <loremipsum.inside@gmail.com>
2020-10-10 19:42:36 +0300

    feat: Add possibility to set any type as the message type for subscription

--- src/ecs/Subscription.ts
+++ src/ecs/Subscription.ts
@@ -5,11 +5,11 @@ import {Class} from '../utils/Class';
  */
 export class Subscription<T> {
   public constructor(
-    public readonly messageType: Class<T>,
+    public readonly messageType: Class<T> | T,
     public readonly handler: (message: T) => void,
   ) {}
 
-  public equals(messageType: Class<T>, handler?: (message: T) => void) {
+  public equals(messageType: Class<T> | T, handler?: (message: T) => void) {
     return this.messageType === messageType && (handler === undefined || this.handler === handler);
   }
 }
